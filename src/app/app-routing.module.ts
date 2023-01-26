ba0a52817a45ed0589924ed592382632d3ebaa64
Iuri <iurirodrigues2200@gmail.com>
2023-01-26 10:18:08 -0300

    feat: add new page Create Account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
 import { ChooseBankComponent } from './choose-bank/choose-bank.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'choose-bank', component: ChooseBankComponent },
+  { path: 'create-account', component: CreateAccountComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ChooseBankComponent } from './choose-bank/choose-bank.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, ChooseBankComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ChooseBankComponent,
+    CreateAccountComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [],
   bootstrap: [AppComponent],
